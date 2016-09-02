18703b5a0db7871c9a431c2a2a751ea7a31e9590
Lauren Oh <OhL4928@sduhsd.lan>
2016-09-02 13:12:44 -0700

    Added Files

---
+++ APLesson_01/ASCIIArt.java
@@ -0,0 +1,21 @@
+public class ASCIIArt
+{
+	public static void main(String[]args)
+	{
+		System.out.println("       Angel"); 
+		System.out.println("        -=-    ");
+		System.out.println(" (\\      _      /)");
+		System.out.println(" ( \\   (   )   / )");
+		System.out.println(" (                )");
+		System.out.println("   \'>         <\'  ");
+		System.out.println("  /              \\");
+		System.out.println("   \'  - . _ .-   \' ");
+		System.out.println(" ");
+		System.out.println("Gingerbread Man");
+		System.out.println(" ");
+		System.out.println("    _( \" )_        ");
+		System.out.println("   (_  .  _)     ");
+		System.out.println("     /  :  \\       ");
+		System.out.println("   (_/   \\_)         ");
+	}
+}
